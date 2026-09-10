8b1a5eb88fca47fdd58367a7024c63a8be1d0a4c
Riduan Aziz <riduan.aziz46@gmail.com>
2026-09-10 06:57:26 +0000

    Elevate portfolio for IT and technology expertise

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ document.addEventListener("DOMContentLoaded", () => {
     magneticButtons.forEach(btn => {
         btn.addEventListener('mousemove', (e) => {
             const position = btn.getBoundingClientRect();
-            const x = e.pageX - position.left - position.width / 2;
-            const y = e.pageY - position.top - position.height / 2;
+            const x = e.clientX - position.left - position.width / 2;
+            const y = e.clientY - position.top - position.height / 2;
             
             // Move button slightly towards cursor
             btn.style.transform = `translate(${x * 0.15}px, ${y * 0.15}px)`;
